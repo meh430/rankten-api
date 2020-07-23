7b725236f39cb17a83bbe55adc9e0f8626356138
Mehul Pillai <mehulpillai30@gmail.com>
2020-07-23 15:14:22 -0400

    get follower list

--- endpoints/follow_ep.py
+++ endpoints/follow_ep.py
@@ -1,4 +1,4 @@
-from flask import Response, request
+from flask import Response, request, jsonify
 from database.models import User
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
@@ -35,3 +35,27 @@ class FollowApi(Resource):
                          followers_num=other.followers_num - 1)
 
         return ('followed user' if exec_follow else 'unfollowed user'), 200
+
+
+class FollowingApi(Resource):
+    def get(self, name):
+        following = User.objects.get(user_name=name).following
+        following_list = []
+        for f in following:
+            following_list.append({
+                user_name: f.user_name
+            })
+
+        return Response(jsonify(following_list), mimetype='application/json', status=200)
+
+
+class FollowersApi(Resource):
+    def get(self, name):
+        followers = User.objects.get(user_name=name).followers
+        followers_list = []
+        for f in followers:
+            followers_list.append({
+                user_name: f.user_name
+            })
+
+        return Response(jsonify(followers_list), mimetype='application/json', status=200)
